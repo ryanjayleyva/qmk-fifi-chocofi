8a462ffd7b562374c890d72ea66542cceec8c054
RJ <ryanjayleyva@proton.me>
2024-12-27 05:15:40 -0500

    Updated Tap Mod layout

--- keymap.c
+++ keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_NO,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO, GUI_T(kc_A), ALT_T(kc_R), CTRL_T(kc_S), SFT_T(kc_T), KC_G,       KC_M, SFT_T(kc_N), RCTL_T(kc_E), ALT_T(kc_I), RGUI_T(kc_O), KC_NO,
+        KC_NO, LGUI_T(KC_A), ALT_T(KC_R), CTRL_T(KC_S), SFT_T(KC_T), KC_G,       KC_M, SFT_T(KC_N), RCTL_T(KC_E), ALT_T(KC_I), RGUI_T(KC_O), KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
